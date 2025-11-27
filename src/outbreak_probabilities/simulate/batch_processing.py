5ef245a2da68e8634caf144b3f45bd3b1ef0f976
oliverkstaples <oliver.staples@hertford.ox.ac.uk>
2025-11-27 14:20:01 +0000

    Create batch_processing.py

---
+++ src/outbreak_probabilities/simulate/batch_processing.py
@@ -0,0 +1,12 @@
+###
+**3. batch_processing.py**
+
+Purpose: this file will call `generate_single_trajectory` as many times as needed to simulate a user-defined number of tracks. It should also write these results to a .csv or a Python `tempfile`. It should write to a CSV with headers day_1,...,day_max; may also include R.
+
+
+Functions:
+- batch_process()
+  
+  - Input: the number of tracks `N`, and the maximum number of days.
+  - Output: a 2D array, printed to a csv.
+###
