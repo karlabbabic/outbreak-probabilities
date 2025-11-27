b8f522999d717386c28994ac99107a1625458b2c
salmamin1 <salma.amin@dtc.ox.ac.uk>
2025-11-27 16:48:23 +0000

    Clean up comments in simulate.py
    Removed commented-out code and explanations related to biological timing and outbreak probabilities.

--- src/outbreak_probabilities/simulate/simulate.py
+++ src/outbreak_probabilities/simulate/simulate.py
@@ -3,19 +3,6 @@
 
 
 
-# turn biological timing(days) into model weights
-
-# Given R, how compatible is it with the observed case data? (log likehood?)
-  # If the predicted λₜ closely matches the observed counts, log-likelihood is high.
-
-# compute the probability that an infection chain eventually dies out (extinction probability for each R)
-
-# the probability of a major outbreak, conditional on R, given 3 weeks of observed case numbers (PMO = 1 - none_prob)
-  # Given R and the 3 weeks of observed cases, what is the probability that all transmission chains will die out?
-
-# produce the overall probability of a major outbreak using the likelihood of R, the extinction probability for each R, a prior over R
-  # full Bayesian integration
-
 
 
 
