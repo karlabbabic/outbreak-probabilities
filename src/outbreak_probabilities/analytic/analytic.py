ee84a3f9771fb613ee25a379594169ac9c7000f1
salmamin1 <salma.amin@dtc.ox.ac.uk>
2025-11-27 11:23:43 +0000

    Add comments to analytic.py
    Add comments to clarify the purpose of the code.

--- src/outbreak_probabilities/analytic/analytic.py
+++ src/outbreak_probabilities/analytic/analytic.py
@@ -1,1 +1,2 @@
-
+# use the firt k day from simulated data and estimated range of reproduction number to predict overall outbreak probability 
+# Also output conditional probability, outbreak probability given first three cases, outbreak probability given reproduction number 
