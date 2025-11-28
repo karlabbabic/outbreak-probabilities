7c518501ccdf0886e38e4d5a5220cdf1060a2d0d
Oliver Staples <oliver.staples@hertford.ox.ac.uk>
2025-11-28 10:19:18 +0000

    Commented

--- src/outbreak_probabilities/simulate/calculate_serial_weights.py
+++ src/outbreak_probabilities/simulate/calculate_serial_weights.py
@@ -1,10 +1,11 @@
-###
-1. calculate_serial_weights.py
+# ###
+# 1. calculate_serial_weights.py
 
-Purpose: this will compute discrete-time serial interval weights w_k from a continuous gamma distribution g(u) 
-Functions: 
-- compute_serial_weights()
+# Purpose: this will compute discrete-time serial interval weights w_k from a continuous gamma distribution g(u) 
+# Functions: 
+# - compute_serial_weights()
   
-  - Input: mean, std of disease (this is the continuous serial interval). Maximum number of days.
-  - Output: weights, `w`, an array of integers of size k_max; array of discrete-week weights. Its important that this sums to one, so use `scipy.signal.normalize` or otherwise.
-###
+#   - Input: mean, std of disease (this is the continuous serial interval). Maximum number of days.
+#   - Output: weights, `w`, an array of integers of size k_max; array of discrete-week weights. Its important that this sums to one, so use `scipy.signal.normalize` or otherwise.
+# ###
+
