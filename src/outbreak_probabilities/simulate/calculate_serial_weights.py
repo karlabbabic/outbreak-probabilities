6ccd1df8cc66537606d6882fddbc76b6471a3f4e
Oliver Staples <oliver.staples@hertford.ox.ac.uk>
2025-11-28 11:33:36 +0000

    wrote main function with quadrature

--- src/outbreak_probabilities/simulate/calculate_serial_weights.py
+++ src/outbreak_probabilities/simulate/calculate_serial_weights.py
@@ -11,8 +11,12 @@
 
 import numpy as np
 from scipy.stats import gamma
+from functools import lru_cache
+from numpy.polynomial.legendre import leggauss
 
-def compute_serial_weights(mean, std, k_max):
+# Use 64 leggauss nodes
+@lru_cache(maxsize=64)
+def compute_serial_weights(mean, std, k_max, nquad=32, step = 7.0):
     
     # Raise some errors
     if k_max < 1:
@@ -25,7 +29,7 @@ def compute_serial_weights(mean, std, k_max):
 
     # Calculate the probability density function g(x) using given params
     var = std ** 2
-    alpha = mean ** 2
+    alpha = (mean/std) ** 2
     theta = var / mean
 
     g = gamma(a = alpha, scale = theta)
@@ -34,13 +38,53 @@ def compute_serial_weights(mean, std, k_max):
     if k_max == 1:
         return np.array([1.0], dtype=float)
 
-    # Compute for k = 2,...,k_max
 
-    # Define k's. An 2d array of floats
-    k = np.arrange(2, k_max+1)
+    # The integral is w_k = int_(k-1)^(k+1)[1-|u-k|]g(u)du; compute for k = 2,...,k_max
+    # quad() is slow because it evaluates g(u) until a certain tolerance is met; instead use Gauss–Legendre quadrature
+    # Instead evaluate the integrand at fixed points; this is accurate for smooth integrals
+    # This gives w_k = int_(left)^(right)[1-|u-center|/step]g(u)du, with:
+    #   center = step * k
+    #   left = step * (k-1)
+    #   right = step * (k-1)
+    # and step = 7 days. 
 
+    nodes, weights = leggauss(nquad)
+
+    for k in range(1,k_max+1):
+        
+        # Calculate the steps
+        center = step * k
+        left = step * (k-1)
+        right = step * (k+1)
+        half_width = 0.5 * (right - left)  # = step
+        midpoint = 0.5 * (right + left)
+
+        # u is where the integral is being evaluated 
+        u = half_width * nodes + midpoint
+
+        # Triangular kernel calculation
+        tri = 1.0 - np.abs(u - center) / step
+        # Remove any -ve numbers
+        tri[tri < 0.0] = 0.0
+
+        # Define gamma pdf
+        pdf_vals = g.pdf(u)
+
+        # Find integrand
+        integrand_vals = tri * pdf_vals
+
+        # Quadrature calculation
+        w_k = half_width * np.sum(weights * integrand_vals)
+        w[k - 1] = w_k
+
+
+    total = float(w.sum())
+    
+    if total <= 0:
+        raise RuntimeError("Weights sum to a -ve number")
+    
     # Normalize w so that they sum to 1
-    w = w.sum()
+    w = w / total
     return w
 
 print(compute_serial_weights(1,2,3))
