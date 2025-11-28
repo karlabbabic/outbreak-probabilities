483c0a6d7782237e388b291aa9ffd0a616a4e846
Oliver Staples <oliver.staples@hertford.ox.ac.uk>
2025-11-28 10:58:22 +0000

    Adding compute_serial_weights

--- src/outbreak_probabilities/simulate/calculate_serial_weights.py
+++ src/outbreak_probabilities/simulate/calculate_serial_weights.py
@@ -9,3 +9,38 @@
 #   - Output: weights, `w`, an array of integers of size k_max; array of discrete-week weights. Its important that this sums to one, so use `scipy.signal.normalize` or otherwise.
 # ###
 
+import numpy as np
+from scipy.stats import gamma
+
+def compute_serial_weights(mean, std, k_max):
+    
+    # Raise some errors
+    if k_max < 1:
+        raise ValueError("k_max must be > 1")
+    
+    # A few more errors to consider: k_max has to be an integer, mean & std data type
+
+    # Instantiate array of weights
+    w = np.zeros(k_max)
+
+    # Calculate the probability density function g(x) using given params
+    var = std ** 2
+    alpha = mean ** 2
+    theta = var / mean
+
+    g = gamma(a = alpha, scale = theta)
+
+    # k_1 is always 1
+    if k_max == 1:
+        return np.array([1.0], dtype=float)
+
+    # Compute for k = 2,...,k_max
+
+    # Define k's. An 2d array of floats
+    k = np.arrange(2, k_max+1)
+
+    # Normalize w so that they sum to 1
+    w = w.sum()
+    return w
+
+print(compute_serial_weights(1,2,3))
