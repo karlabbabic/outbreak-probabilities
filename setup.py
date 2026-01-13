c792d3a70001a954a72ba72100494671491990f5
Salma Amin <salma.amin@dtc.ox.ac.uk>
2026-01-13 14:15:34 +0000

    update version_info

--- setup.py
+++ setup.py
@@ -12,8 +12,11 @@ def get_version():
     """
     import os
     import sys
+    # it should be in src/outbreak_probabilities/version_info.py
+    # so we add src/outbreak_probabilities to the path
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src', 'outbreak_probabilities'))
     
-    from version_info import VERSION as version
+    from src.outbreak_probabilities.version_info import VERSION as version
     sys.path.pop()
 
     return version
